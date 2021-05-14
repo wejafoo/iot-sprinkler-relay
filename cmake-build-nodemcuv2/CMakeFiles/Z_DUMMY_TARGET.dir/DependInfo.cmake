624003dc77955f414005302518f425d3dd7ead56
wejafoo <wes@weja.us>
2021-05-13 19:10:19 -0600

    Initial commit

---
+++ cmake-build-nodemcuv2/CMakeFiles/Z_DUMMY_TARGET.dir/DependInfo.cmake
@@ -0,0 +1,61 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/wes/local-dev/projects-wes/wj/clion/platformio/sprinkler/src/main.cpp" "/Users/wes/local-dev/projects-wes/wj/clion/platformio/sprinkler/cmake-build-nodemcuv2/CMakeFiles/Z_DUMMY_TARGET.dir/src/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "../src"
+  "/Users/wes/.platformio/packages/framework-arduinoespressif8266/libraries/ESP8266WebServer/src"
+  "/Users/wes/.platformio/packages/framework-arduinoespressif8266/libraries/ESP8266WiFi/src"
+  "/Users/wes/.platformio/packages/framework-arduinoespressif8266/tools/sdk/include"
+  "/Users/wes/.platformio/packages/framework-arduinoespressif8266/tools/sdk/libc/xtensa-lx106-elf/include"
+  "/Users/wes/.platformio/packages/framework-arduinoespressif8266/cores/esp8266"
+  "/Users/wes/.platformio/packages/framework-arduinoespressif8266/tools/sdk/lwip2/include"
+  "/Users/wes/.platformio/packages/framework-arduinoespressif8266/variants/nodemcu"
+  "/Users/wes/.platformio/packages/framework-arduinoespressif8266/libraries/ArduinoOTA"
+  "/Users/wes/.platformio/packages/framework-arduinoespressif8266/libraries/DNSServer/src"
+  "/Users/wes/.platformio/packages/framework-arduinoespressif8266/libraries/EEPROM"
+  "/Users/wes/.platformio/packages/framework-arduinoespressif8266/libraries/ESP8266AVRISP/src"
+  "/Users/wes/.platformio/packages/framework-arduinoespressif8266/libraries/ESP8266HTTPClient/src"
+  "/Users/wes/.platformio/packages/framework-arduinoespressif8266/libraries/ESP8266HTTPUpdateServer/src"
+  "/Users/wes/.platformio/packages/framework-arduinoespressif8266/libraries/ESP8266LLMNR"
+  "/Users/wes/.platformio/packages/framework-arduinoespressif8266/libraries/ESP8266NetBIOS"
+  "/Users/wes/.platformio/packages/framework-arduinoespressif8266/libraries/ESP8266SSDP"
+  "/Users/wes/.platformio/packages/framework-arduinoespressif8266/libraries/ESP8266SdFat/src"
+  "/Users/wes/.platformio/packages/framework-arduinoespressif8266/libraries/ESP8266WiFiMesh/src"
+  "/Users/wes/.platformio/packages/framework-arduinoespressif8266/libraries/ESP8266httpUpdate/src"
+  "/Users/wes/.platformio/packages/framework-arduinoespressif8266/libraries/ESP8266mDNS/src"
+  "/Users/wes/.platformio/packages/framework-arduinoespressif8266/libraries/Ethernet/src"
+  "/Users/wes/.platformio/packages/framework-arduinoespressif8266/libraries/GDBStub/src"
+  "/Users/wes/.platformio/packages/framework-arduinoespressif8266/libraries/Hash/src"
+  "/Users/wes/.platformio/packages/framework-arduinoespressif8266/libraries/LittleFS/src"
+  "/Users/wes/.platformio/packages/framework-arduinoespressif8266/libraries/SD/src"
+  "/Users/wes/.platformio/packages/framework-arduinoespressif8266/libraries/SDFS/src"
+  "/Users/wes/.platformio/packages/framework-arduinoespressif8266/libraries/SPI"
+  "/Users/wes/.platformio/packages/framework-arduinoespressif8266/libraries/SPISlave/src"
+  "/Users/wes/.platformio/packages/framework-arduinoespressif8266/libraries/Servo/src"
+  "/Users/wes/.platformio/packages/framework-arduinoespressif8266/libraries/SoftwareSerial/src"
+  "/Users/wes/.platformio/packages/framework-arduinoespressif8266/libraries/TFT_Touch_Shield_V2"
+  "/Users/wes/.platformio/packages/framework-arduinoespressif8266/libraries/Ticker/src"
+  "/Users/wes/.platformio/packages/framework-arduinoespressif8266/libraries/Wire"
+  "/Users/wes/.platformio/packages/framework-arduinoespressif8266/libraries/esp8266/src"
+  "/Users/wes/.platformio/packages/toolchain-xtensa/xtensa-lx106-elf/include/c++/4.8.2"
+  "/Users/wes/.platformio/packages/toolchain-xtensa/xtensa-lx106-elf/include/c++/4.8.2/xtensa-lx106-elf"
+  "/Users/wes/.platformio/packages/toolchain-xtensa/lib/gcc/xtensa-lx106-elf/4.8.2/include"
+  "/Users/wes/.platformio/packages/toolchain-xtensa/lib/gcc/xtensa-lx106-elf/4.8.2/include-fixed"
+  "/Users/wes/.platformio/packages/toolchain-xtensa/xtensa-lx106-elf/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
